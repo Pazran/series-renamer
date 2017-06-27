88ab2245861714fd0e8e8d01759fd1411d8d5692
Pazran <lawrenerno@gmail.com>
2017-06-27 18:40:10 +0800

    add seasonal TV Show naming style and pattern

--- rename.py
+++ rename.py
@@ -3,33 +3,58 @@ import re
 import sys
 import logging
 import tvdb_api
+from os.path import basename
 
-TITLE = "Hunter x Hunter (2011)"
+TITLE = "Seven Mortal Sins"
+NAME_FORMAT = "Seven Mortal Sins"
 SEASON = 1
-NAME_FORMAT = 'HxH'
+
+## choose style
+## Anime Style 1 = NAME_FORMAT + EP + EPISODE NAME
+## TV Show Style 2 = NAME_FORMAT + SXXEXX + EPNAME
+
+STYLE = 1
+rename = False
 
 def getInfo(fpath):
     
     try:
+
         t = tvdb_api.Tvdb()
         root, ext = os.path.splitext(fpath)
         consPath, consDir, consFile = next(os.walk(os.path.dirname(fpath)))
-        pattern = ["(-+)(\d+)(-+)","(-+|\s|_+)(ep\d+|EP\d+|\d+)(-+|\s|_+)"] # AnimeHeaven files name pattern
+        pattern = ["(-+)(\d+)(-+)","(-+|\s|_+)(ep\d+|EP\d\+|\d+)(-+|\s|_+)",
+                   "(s\d+|S\d+)(e\d+|E\d+)","\s\d+(\s|.)","(-+)(\d+)","Ep[.-](\d+)"] # Filename patterns
+
+        ### Search if filename pattern exist in pattern list ###
 
         for pt in pattern:
-            result = re.search(pt,fpath)
+            result = re.search(pt,basename(root))
             if result:
-                rawEp = (result.group()).lower()         
-                ep = rawEp.replace('-','').replace('_','').replace('ep','').replace('-','')
+                rawEp = (result.group()).lower()
+                ep = rawEp.replace('-','').replace('_','').replace('ep','').replace("s" + "{0:0=2d}".format(SEASON), '').replace('e','').replace('-','').replace('.','')
+                rename = True
+                print("Renaming episode: " + ep)
+                break # stop the pattern search after it found one to prevent duplicate
 
-        if ext not in [".avi", ".mp4", ".mkv", ".mpg", ".mpeg", ".mov", ".rm", ".vob", ".wmv", ".flv", ".3gp",".3g2"]:
+            elif result == None:
+                rename = False
+
+        if ext not in [".avi", ".mp4", ".mkv", ".mpg", ".mpeg", ".mov", ".rm", ".vob", ".wmv", ".flv", ".3gp",".3g2" , ".srt"]:
             return
-        
-        if result or result2:
-            print(ep)
+
+        ### rename = True if one of the filename pattern exist ###
+
+        if rename:
             info = t[TITLE][SEASON][int(ep)]
-            epName = info['episodename'].replace('?','').replace('!','')
-            newFileName = NAME_FORMAT + " " +str("{0:0=3d}".format(int(ep))) + ' - ' + epName + ext
+            epName = info['episodename'].replace('?','').replace(':','')
+
+            ### Naming style. Anime or Season TV Show. ###
+
+            if STYLE == 1:
+                newFileName = NAME_FORMAT + " " + str("{0:0=3d}".format(int(ep))) + ' - ' + epName + ext
+            elif STYLE == 2:
+                newFileName = NAME_FORMAT + " S" + str("{0:0=2d}".format(int(SEASON))) + "E" + str("{0:0=2d}".format(int(ep))) + " - " + epName + ext
 
             if (fpath != os.path.join(consPath, newFileName)):
                 os.rename(fpath, os.path.join(consPath, newFileName))
@@ -52,6 +77,8 @@ def main():
     if len(sys.argv) == 1:
         print("This program require one parameter!")
         sys.exit(1)
+
+    ### Iterate for multiple files
 
     for path in sys.argv[1:]:
         if os.path.isdir(path):
